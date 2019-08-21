a954136d15053c6d6aed5a4eaa5a80d554369a79
Weizhen Wang <wangwzhen@gmail.com>
2019-08-21 13:56:02 -0500

    create routes

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+// import {Navbar} from 'react-bootstrap';
+
+import ExerciseList from '../ExerciseList/ExerciseList';
+import CreateUser from '../CreateUser/CreateUser';
+import CreateExercise from '../CreateExercise/CreateExercise';
+
+function App() {
+  return (
+    <div className="App">
+      Exercise Tracker
+      <Router>
+        <Route path="/exercise" exact component={ExerciseList} />
+        <Route path="/user" exact component={CreateUser} />
+        <Route path="/new" component={CreateExercise} />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
